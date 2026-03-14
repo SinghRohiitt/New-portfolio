74013d5f2359f8b72d685c00353be6661a7d3783
Rohit Singh <singhrohit004008@gmail.com>
2026-03-14 16:21:54 +0530

    navbar and the social icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,14 +43,14 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          RC
+          Rohit Singh
         </a>
         <a
-          href="mailto:rajeshchittyal21@gmail.com"
+          href="mailto:singhrohit004008@gmail.com"
           className="navbar-connect"
           data-cursor="disable"
         >
-          rajeshchittyal21@gmail.com
+          singhrohit004008@gmail.com
         </a>
         <ul>
           <li>
